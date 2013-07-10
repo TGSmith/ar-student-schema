8857c828e1a4a982c7eaec424db876bc24182aed
TGSmith <Tristan13smith@gmail.com>
2013-07-10 17:09:15 -0500

    Update 20121011144238_create_students.rb

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,16 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table(:students) do |t|
+      t.column :first_name, :string
+      t.column :last_name,  :string
+      t.column :gender,     :string
+      t.column :email,      :string
+      t.column :phone,      :string
+      t.column :birthday,   :date
+    end
   end
 end
+
+
+
